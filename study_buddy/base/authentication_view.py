e0415a9ade9a8efbffcd23b9aeb463131cd60f1e
raghavm8 <raghav.mittal1109@gmail.com>
2025-10-03 15:47:56 +0530

    authenticaton dine

--- study_buddy/base/authentication_view.py
+++ study_buddy/base/authentication_view.py
@@ -5,35 +5,60 @@ from django.contrib import messages
 from django.contrib.auth import authenticate
 from django.contrib.auth import login
 from django.contrib.auth import logout
+from django.contrib.auth.forms import UserCreationForm
 
+    
 def login_page(request):
     print("inside login")
+
+    if request.user.is_authenticated:
+        return redirect("Home")
+
     if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        username = request.POST.get("username").lower()
+        password = request.POST.get("password")
         try:
             user = User.objects.get(username=username)
         except:
             print("User does not exist")
             messages.error(request, "User does not exist")
-        
+
         user = authenticate(request, username=username, password=password)
-        
+
         if user is not None:
             login(request, user)
             return redirect("Home")
         else:
             print(user)
             messages.error(request, "Username or password does not exist")
-        
+
     context = {}
     return render(request, "login.html", context)
 
+
 def register_page(request):
-    context = {}
-    return render(request, "login.html", context)
+    form = UserCreationForm()
+    
+    if request.user.is_authenticated:
+        return redirect("Home")
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST) 
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            print(user.username)
+            user.save()
+            login(request, user)
+            return redirect("Home")
+        else: 
+            messages.error(request, "An error occured during registration")
+            
+            
+    context = {"form": form}
+    return render(request, "register.html", context)
+
 
 def logout_page(request):
     logout(request)
     return redirect("LoginUser")
-
